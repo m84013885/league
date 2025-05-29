05ac38bd6c99a70fe7f4e3756ebc764cc2fafe4b
kimingw <m84013885@163.com>
2025-05-29 09:59:27 +0800

    fix : build

--- app/types.ts
+++ app/types.ts
@@ -1,18 +1,20 @@
 export type ShotType = '2p' | '3p' | 'ft' | 'foul' | 'flagrant';
 
+export interface PlayerStats {
+    totalScore: number;
+    fouls: number;
+    flagrantFouls: number;
+    attempts: {
+        '2p': { made: number; total: number; };
+        '3p': { made: number; total: number; };
+        'ft': { made: number; total: number; };
+    };
+}
+
 export interface ScoreHistory {
     player: string;
     type: ShotType;
     isSuccess: boolean;
     isTeam1: boolean;
-    previousStats: {
-        totalScore: number;
-        fouls: number;
-        flagrantFouls: number;
-        attempts: {
-            '2p': { made: number; total: number; };
-            '3p': { made: number; total: number; };
-            'ft': { made: number; total: number; };
-        };
-    };
+    previousStats: PlayerStats;
 } 
